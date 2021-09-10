1763877d59d46a84d69bd8087fdb67c927dae7c2
kaizen3031593 <zen.conroy@gmail.com>
2021-09-10 13:58:17 -0400

    initial step function state machine

--- lib/sfn-glue-ref-app-stack.ts
+++ lib/sfn-glue-ref-app-stack.ts
@@ -1,9 +1,28 @@
 import * as cdk from '@aws-cdk/core';
+import * as lambda from '@aws-cdk/aws-lambda';
+import * as sfn from '@aws-cdk/aws-stepfunctions';
+import * as tasks from '@aws-cdk/aws-stepfunctions-tasks';
+
 
 export class SfnGlueRefAppStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const helloFunction = new lambda.Function(this, 'MyLambdaFunction', {
+      code: lambda.Code.fromInline(`
+          exports.handler = (event, context, callback) => {
+            callback(null, "Hello World!");
+          };
+      `),
+      runtime: lambda.Runtime.NODEJS_12_X,
+      handler: "index.handler",
+      timeout: cdk.Duration.seconds(25),
+    });
+
+    const stateMachine = new sfn.StateMachine(this, 'MyStateMachine', {
+      definition: new tasks.LambdaInvoke(this, "MyLambdaTask", {
+        lambdaFunction: helloFunction
+      }).next(new sfn.Succeed(this, "GreetedWorld")),
+    });
   }
 }
